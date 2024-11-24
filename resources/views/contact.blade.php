1d014e25c02c8d55ea5798ccd70ab64784478ba6
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-24 11:06:44 -0300

    day 04

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
+    <x-slot:heading>
+        Contact page
+    </x-slot:heading>
+
     <h1>Contact!</h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,7 @@
-<x-layout>
+<x-layout heading="Home">
+    <x-slot:heading>
+        Home page
+    </x-slot:heading>
+
     <h1>Hello from Home</h1>
 </x-layout>
