91401aa2593d323860dda24546a3382877d66984
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-28 10:23:35 -0300

    day 16

--- routes/web.php
+++ routes/web.php
@@ -9,19 +9,35 @@ Route::get('/', function () {
 
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->paginate(5);
+    $jobs = Job::with('employer')->latest()->paginate(5);
     // $jobs = Job::with('employer')->simplePaginate(5);
     // $jobs = Job::with('employer')->cursorPaginate(5); -> usado em infite scrolling, por exemplo e bases maiores de dados
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' =>  $jobs
     ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-   return view('job', ['job' => $job]);
+   return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+    // validation
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
