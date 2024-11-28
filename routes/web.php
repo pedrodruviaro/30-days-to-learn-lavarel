83f1d92166a285026a05996478f4db4086223e7f
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-28 09:04:00 -0300

    day 14

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,9 @@ Route::get('/', function () {
 
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->get();
+    $jobs = Job::with('employer')->paginate(5);
+    // $jobs = Job::with('employer')->simplePaginate(5);
+    // $jobs = Job::with('employer')->cursorPaginate(5); -> usado em infite scrolling, por exemplo e bases maiores de dados
 
     return view('jobs', [
         'jobs' =>  $jobs
