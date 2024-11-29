354b0d54fa022815dfe5d7497becd89c8380fa07
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-29 09:34:04 -0300

    day 18

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,7 +3,10 @@
         Job
     </x-slot:heading>
 
-    <h2 class="font-bold text-2xl mb-2">{{ $job['title'] }}</h2>
-    <p>This job pays {{ $job['salary'] }} for year</p>
+    <h2 class="font-bold text-2xl mb-2">{{ $job->title }}</h2>
+    <p>This job pays {{ $job->salary }} for year</p>
 
+    <p class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+    </p>
 </x-layout>
