d570366c1547b130c403bdab43fbed7f0ae8049d
pedrodruviaro <pedrodruviaro@gmail.com>
2024-12-03 14:58:49 -0300

    day 25

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Jobs\TranslateJob;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
@@ -31,5 +32,6 @@ Route::post('/logout', [SessionController::class, 'destroy']);
 
 
 Route::get('/test', function () {
+    TranslateJob::dispatch();
     return 'Done';
 });
